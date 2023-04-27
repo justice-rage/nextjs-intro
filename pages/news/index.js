4a8956fba2197fc750d0e856f2ca1f67f32f0412
justicepelteir <justice.douglas@hey.com>
2023-04-26 19:17:17 -0700

    add link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,20 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 // our-domain.com/news/something-important
 
 function NewsPage() {
-    return <h1>The News Page</h1>
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/test1">test1</Link>
+        </li>
+        <li>test2</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
